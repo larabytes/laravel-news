34d838f8b9ac95a4a18469724fafcb794d2b3457
KaniRobinson <me@kanirobinson.co.uk>
2020-09-11 02:23:36 +0100

    Initial commit of the laravel news aggregator

---
+++ src/Aggregator.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Aggregators\LaravelNews;
+
+use Carbon\Carbon;
+use InvalidArgumentException;
+use Aggregators\Support\BaseAggregator;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Aggregator extends BaseAggregator
+{
+    /**
+     * {@inheritDoc}
+     */
+    public string $uri = 'https://laravel-news.com/category/news';
+
+    /**
+     * {@inheritDoc}
+     */
+    public string $provider = 'Laravel News';
+
+    /**
+     * {@inheritDoc}
+     */
+    public string $logo = 'logo.png';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function articleIdentifier(): string
+    {
+        return 'div.card.w-full.card--post.mx-0';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function nextUrl(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('a.btn.px-4.py-2.text-xs.arrow--right.bg-red.transition')->first()->attr('href');
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function image(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('div.post__image img')->attr('src');
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function title(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('h2.text-2xl')->text();
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function content(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('p')->text();
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function link(Crawler $crawler): ?string
+    {
+        try {
+            return 'https://laravel-news.com' . $crawler->filter('a')->attr('href');
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dateCreated(Crawler $crawler): Carbon
+    {
+        try {
+            return Carbon::parse($crawler->filter('span.label.text-xs > span:nth-of-type(2)')->text());
+        } catch (InvalidArgumentException $e) {
+            return Carbon::now();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dateUpdated(Crawler $crawler): Carbon
+    {
+        return $this->dateCreated($crawler);
+    }
+}
